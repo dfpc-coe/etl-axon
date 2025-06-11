2477a57acb5906c3328cb6c7ef639282e2da63e6
ingalls <nick@ingalls.ca>
2025-06-11 09:38:59 -0600

    Update CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,10 @@
 
 ## Version History
 
+### v1.1.0
+
+- :rocket: Data Ingestion working
+
 ### v1.0.0
 
 - :rocket: Initial Approach
--- task.ts
+++ task.ts
@@ -23,11 +23,25 @@ const InputSchema = Type.Object({
     })
 });
 
-/**
- * The Output Schema contains the known properties that will be returned on the
- * GeoJSON Feature in the .properties.metdata object
- */
-const OutputSchema = Type.Object({})
+const OutputSchema = Type.Object({
+    partnerName: Type.String(),
+    axonDeviceId: Type.String(),
+    deviceModel: Type.String(),
+    deviceUpdateTimestamp: Type.Integer(),
+    deviceSerial: Type.String(),
+    location_accuracy: Type.Number(),
+    location_latitude: Type.Number(),
+    location_longitude: Type.Number(),
+    location_locationUpdateTimestamp: Type.Integer(),
+    status: Type.String(),
+    stream_isStreamable: Type.Boolean(),
+    signalStrength: Type.Optional(Type.String()),
+    battery: Type.Optional(Type.Integer()),
+    primary_assignee_firstName: Type.Optional(Type.String()),
+    primary_assignee_lastName: Type.Optional(Type.String()),
+    primary_assignee_badgeNumber: Type.Optional(Type.String()),
+    primary_assignee_userId: Type.Optional(Type.String())
+});
 
 export default class Task extends ETL {
     static name = 'etl-axon'
@@ -53,7 +67,16 @@ export default class Task extends ETL {
         const layer = await this.fetchLayer();
         const env = await this.env(InputSchema);
 
-        if (!layer) {
+        let access_token;
+
+        if (
+            !layer.incoming.ephemeral.access_token
+            || !layer.incoming.ephemeral.access_token_expires
+            // If Token is going to expire within 1 minute, request a new token
+            || (Number(layer.incoming.ephemeral.access_token_expires) + 60000) < +new Date()
+        ) {
+            console.log('ok - Requesting New Token');
+
             const oauthReq = await fetch(`https://${env.AgencyName}.evidence.com/api/oauth2/token`, {
                 method: 'POST',
                 headers: {
@@ -85,16 +108,148 @@ export default class Task extends ETL {
                 access_token: oauthRes.access_token,
                 access_token_expires: String(oauthRes.expires_on)
             })
+
+            access_token = oauthRes.access_token;
+        } else {
+            access_token = layer.incoming.ephemeral.access_token;
         }
 
-        const features: Static<typeof InputFeature>[] = [];
+        console.log('ok - requesting devices');
+        let from = 0;
+        let total = Infinity;
 
-        const fc: Static<typeof InputFeatureCollection> = {
-            type: 'FeatureCollection',
-            features: features
-        }
+        do {
+            const devicesReq = await fetch(`https://${env.AgencyName}.evidence.com/respond/api/v1/devices/states/search`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Client-Type': 'EXTERNAL',
+                    Authorization: `Bearer ${access_token}`
+                },
+                body: JSON.stringify({
+                    from: 0,
+                    size: 2000
+                })
+            });
 
-        await this.submit(fc);
+            const devicesRes = await devicesReq.typed(Type.Object({
+                meta: Type.Object({
+                    correlationId: Type.String(),
+                    serverTimestamp: Type.Integer(),
+                    totalHits: Type.Integer(),
+                    count: Type.Integer()
+                }),
+                data: Type.Array(Type.Object({
+                    partnerId: Type.String(),
+                    partnerName: Type.String(),
+                    axonDeviceId: Type.String(),
+                    deviceModel: Type.String(),
+                    deviceUpdateTimestamp: Type.Integer(),
+                    attributes: Type.Object({
+                        deviceSerial: Type.String(),
+                        location: Type.Object({
+                            accuracy: Type.Number(),
+                            latitude: Type.Number(),
+                            longitude: Type.Number(),
+                            locationUpdateTimestamp: Type.Integer()
+                        }),
+                        status: Type.String(),
+                        stream: Type.Object({
+                            isStreamable: Type.Boolean()
+                        }),
+                        links: Type.Optional(Type.Object({
+                            view: Type.String()
+                        })),
+                        signalStrengths: Type.Optional(Type.Array(Type.Object({
+                            signalStrength: Type.String()
+                        }))),
+                        batteries: Type.Optional(Type.Array(Type.Object({
+                            batteryPercentage: Type.Integer()
+                        }))),
+                        assignees: Type.Optional(Type.Array(Type.Object({
+                            assigneeType: Type.String(),
+                            firstName: Type.String(),
+                            lastName: Type.String(),
+                            badgeNumber: Type.String(),
+                            userId: Type.String(),
+                            primary: Type.Boolean()
+                        })))
+                    })
+                }))
+            }), {
+                verbose: true
+            });
+
+            const features: Static<typeof InputFeature>[] = [];
+
+            for (const device of devicesRes.data) {
+                const primary = (device.attributes.assignees || []).filter((user) => {
+                    return user.primary
+                })
+
+                if (device.attributes.status === 'DOCKED') {
+                    // We don't care about charging devices
+                    continue;
+                } else if (new Date(device.attributes.location.locationUpdateTimestamp).getTime() < new Date().getTime() - (30 * 60 * 1000)) {
+                    // We cut off devices if we haveh't seen them for 30 minutes
+                    continue;
+                }
+
+                const feat: Static<typeof InputFeature> = {
+                    id: device.axonDeviceId,
+                    type: 'Feature',
+                    properties: {
+                        type: 'a-f-G-U-U-L',
+                        how: 'm-g',
+                        callsign: primary.length ? `${primary[0].firstName} ${primary[0].lastName}` : 'Unknown User',
+                        time: new Date().toISOString(),
+                        start: new Date(device.attributes.location.locationUpdateTimestamp).toISOString(),
+                        status: device.attributes.batteries ? {
+                            battery: String(device.attributes.batteries[0].batteryPercentage)
+                        } : undefined,
+                        remarks: [
+                            `Agency: ${device.partnerName}`
+                        ].join('\n'),
+                        metadata: {
+                            partnerName: device.partnerName,
+                            axonDeviceId: device.axonDeviceId,
+                            deviceModel: device.deviceModel,
+                            deviceUpdateTimestamp: device.deviceUpdateTimestamp,
+                            deviceSerial: device.attributes.deviceSerial,
+                            location_accuracy: device.attributes.location.accuracy,
+                            location_latitude: device.attributes.location.latitude,
+                            location_longitude: device.attributes.location.longitude,
+                            location_locationUpdateTimestamp: device.attributes.location.locationUpdateTimestamp,
+                            status: device.attributes.status,
+                            stream_isStreamable: device.attributes.stream ? device.attributes.stream.isStreamable : false,
+                            signalStrength: device.attributes.signalStrengths ? device.attributes.signalStrengths[0].signalStrength : undefined,
+                            battery: device.attributes.batteries ? device.attributes.batteries[0].batteryPercentage : undefined,
+                            primary_assignee_firstName: primary.length ? primary[0].firstName : undefined,
+                            primary_assignee_lastName: primary.length ? primary[0].lastName : undefined,
+                            primary_assignee_badgeNumber: primary.length ? primary[0].badgeNumber : undefined,
+                            primary_assignee_userId: primary.length ? primary[0].userId : undefined
+                        }
+                    },
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [ device.attributes.location.longitude, device.attributes.location.latitude ]
+                    }
+                }
+
+                features.push(feat);
+            }
+
+            const fc: Static<typeof InputFeatureCollection> = {
+                type: 'FeatureCollection',
+                features: features
+            }
+
+            await this.submit(fc);
+
+            total = devicesRes.meta.totalHits;
+            from = from + 2000;
+        } while (total > from)
     }
 }
 
